1135352f75bed4132ca6c09278dec4faeff611c7
John Colvin <colvin.john@gmail.com>
2013-01-11 16:09:32 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Top250instant::Application.config.secret_token = '25d9741fd8095253eeede5341a67a65c1b284052dab6a5884b4d8c17b3bf209dcc9af9e1c717562109e838be48c87e93e9c1f2d6f1ad469107b6cfedab70b066'
