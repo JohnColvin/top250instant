42e5044976be7c5cc9fcd0db1f1a32b21bc84960
John Colvin <colvin.john@gmail.com>
2013-01-12 18:31:20 -0500

    Updated path to get IMDB top 250 due to API changes

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,7 +3,7 @@ class MoviesController < ApplicationController
   caches_action :index
 
   def index
-    @imdb_movies = Movie.all(from: :top)
+    @imdb_movies = Movie.all(from: :top250)
   end
 
 end
