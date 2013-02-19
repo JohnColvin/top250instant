745a97455e07f3b22ee203d3e2499bb7f3512ab1
Pairing Machine <pairing@edgecase.com>
2013-02-19 12:01:36 -0500

    Replaced best picture api call with imdbapi.org

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,14 +7,28 @@ class MoviesController < ApplicationController
   end
 
   def best
-    @imdb_movies = Movie.all(from: :"best-pictures").reverse
+    @imdb_movies = best_picture_winners
   end
 
 
   def top_250
     top_table = Nokogiri::HTML(open('http://www.imdb.com/chart/top')).css('div#main table')[1]
     top_table.children[0].remove #remove header row
-    movies_from_anchor_tags top_table.children[0..1].map{ |row| row.at_css('td a') }
+    movies_from_anchor_tags top_table.children.map{ |row| row.at_css('td a') }
+  end
+
+  def best_picture_winners
+    (1929..Date.today.year).map { |year| best_picture_winner(year) }.compact
+  end
+
+  def best_picture_winner(year)
+    oscar_results_page = Nokogiri::HTML(open("http://www.imdb.com/event/ev0000003/#{year}"))
+    awards_block = oscar_results_page.at_css('#main .award')
+    return nil if awards_block.nil?
+    winner_header = awards_block.at_css('h3')
+    return nil unless winner_header.text.downcase == 'winner'
+    best_picture_anchor_tag = winner_header.next_element.at_css('strong a')
+    movie_from_anchor_tag(best_picture_anchor_tag)
   end
 
   def movies_from_anchor_tags(anchor_tags)
