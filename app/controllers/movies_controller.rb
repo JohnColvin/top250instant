713303b7f81da691c0ce31e53eb51f4d32f72988
John Colvin <john.colvin@neo.com>
2013-02-19 15:13:03 -0500

    Cleaned up the methods used to get best picture and top 250

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,41 +3,11 @@ class MoviesController < ApplicationController
   caches_action :index, :best
 
   def index
-    @imdb_movies = top_250
+    @imdb_movies = Movie.top_250
   end
 
   def best
-    @imdb_movies = best_picture_winners
-  end
-
-
-  def top_250
-    top_table = Nokogiri::HTML(open('http://www.imdb.com/chart/top')).css('div#main table')[1]
-    top_table.children[0].remove #remove header row
-    movies_from_anchor_tags top_table.children.map{ |row| row.at_css('td a') }
-  end
-
-  def best_picture_winners
-    (1929..Date.today.year).map { |year| best_picture_winner(year) }.compact
-  end
-
-  def best_picture_winner(year)
-    oscar_results_page = Nokogiri::HTML(open("http://www.imdb.com/event/ev0000003/#{year}"))
-    awards_block = oscar_results_page.at_css('#main .award')
-    return nil if awards_block.nil?
-    winner_header = awards_block.at_css('h3')
-    return nil unless winner_header.text.downcase == 'winner'
-    best_picture_anchor_tag = winner_header.next_element.at_css('strong a')
-    movie_from_anchor_tag(best_picture_anchor_tag)
-  end
-
-  def movies_from_anchor_tags(anchor_tags)
-    anchor_tags.map { |anchor_tag| movie_from_anchor_tag(anchor_tag) }
-  end
-
-  def movie_from_anchor_tag(anchor_tag)
-    anchor_tag.attribute('href').to_s.match(/(tt[\d]+)/)
-    Movie.new($1)
+    @imdb_movies = Movie.best_picture_winners
   end
 
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,10 +1,7 @@
 class Movie
   attr_reader :title, :release_year, :storyline
 
-  def initialize(id)
-    uri = URI.parse("http://imdbapi.org?type=json&id=#{id}")
-    result = Net::HTTP.get(uri)
-    data = JSON.parse(result)
+  def initialize(data)
     @title = data['title']
     @release_year = data['year']
     @storyline = data['plot_simple']
@@ -30,6 +27,25 @@ class Movie
     storyline
   end
 
+  def self.top_250
+    top_table = Nokogiri::HTML(open('http://www.imdb.com/chart/top')).css('div#main table')[1]
+    top_table.children[0].remove #remove header row
+    top_250_ids = imdb_ids_from_anchor_tags(top_table.children.map{ |row| row.at_css('td a') })
+    fetch(top_250_ids)
+  end
+
+  def self.best_picture_winners
+    best_picture_ids = (1929..Date.today.year).map{ |year| best_picture_winner_id(year) }.compact
+    fetch(best_picture_ids)
+  end
+
+  def self.fetch(imdb_ids)
+    uri = URI.parse("http://imdbapi.org?type=json&ids=#{imdb_ids.join(',')}")
+    result = Net::HTTP.get(uri)
+    data = JSON.parse(result)
+    data.map{ |movie_data| Movie.new(movie_data) }
+  end
+
   private
 
   def netflix_title
@@ -37,6 +53,24 @@ class Movie
       results = NetFlix::Title.search(term: title, max_results: 10)
       results.find{|nf_title| nf_title.title == title && nf_title.release_year.to_i == release_year}
    end
+  end
+
+  def self.best_picture_winner_id(year)
+    oscar_results_page = Nokogiri::HTML(open("http://www.imdb.com/event/ev0000003/#{year}"))
+    awards_block = oscar_results_page.at_css('#main .award')
+    return nil if awards_block.nil?
+    winner_header = awards_block.at_css('h3')
+    return nil unless winner_header.text.downcase == 'winner'
+    best_picture_anchor_tag = winner_header.next_element.at_css('strong a')
+    imdb_id_from_anchor_tag(best_picture_anchor_tag)
+  end
+
+  def self.imdb_id_from_anchor_tag(anchor_tag)
+    anchor_tag.attribute('href').to_s.match(/(tt[\d]+)/)
+  end
+
+  def self.imdb_ids_from_anchor_tags(anchor_tags)
+    anchor_tags.map{ |anchor_tag| imdb_id_from_anchor_tag(anchor_tag) }
   end
 
 end
