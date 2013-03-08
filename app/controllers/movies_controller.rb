6cb42d1f29d81313301e22496f6c6fef293f1846
John Colvin <john.colvin@neo.com>
2013-03-08 11:32:57 -0500

    Set active class on current sort link

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,6 +7,7 @@ class MoviesController < ApplicationController
 
     @imdb_movies = @imdb_movies.order("#{params[:sort]} asc") if %w{ title release_year length mpaa_rating }.include?(params[:sort])
     @imdb_movies = @imdb_movies.order('imdb_ranking asc')
+    params[:sort] = 'imdb_ranking' if params[:sort].blank?
   end
 
 end
